312bdd46c244adbb057707a6f0e4ab7a1655d838
Shubham Bandagale <sbandagale18@gmail.com>
2021-08-16 02:35:48 +0530

    UC-1

---
+++ src/com/bridgelabz/GamblingSimulation.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class GamblingSimulation {
+    public static final int STAKE = 100;
+    public static final int BET = 1;
+
+    public static void main(String[] args) {
+
+        System.out.println("WELCOME TO GAMBLING SIMULATION PROGRAM");
+
+        System.out.println("Stake:" + STAKE + " $");
+        System.out.println("Bet:" + BET + " $");
+    }
+}
