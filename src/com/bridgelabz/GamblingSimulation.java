53c1cb0633088e5b326195f471e31168403c66dd
Shubham Bandagale <sbandagale18@gmail.com>
2021-08-16 02:48:10 +0530

    UC-2

--- src/com/bridgelabz/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation.java
@@ -1,14 +1,26 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class GamblingSimulation {
     public static final int STAKE = 100;
     public static final int BET = 1;
 
+    /**
+     * @UC-2 :Win or Lose
+     */
+    public void winOrLoss(){
+        Random random = new Random();
+        int value= random.nextInt(2);
+        if(value == 1)
+            System.out.println("WON BET");
+        else
+            System.out.println("LOST BET");
+    }
+
     public static void main(String[] args) {
-
+        GamblingSimulation gamblerSimulation = new GamblingSimulation();
         System.out.println("WELCOME TO GAMBLING SIMULATION PROGRAM");
-
-        System.out.println("Stake:" + STAKE + " $");
-        System.out.println("Bet:" + BET + " $");
+        gamblerSimulation.winOrLoss();
     }
 }
