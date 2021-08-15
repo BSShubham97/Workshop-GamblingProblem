42897984bfbd12062f81b3bce81c898d544e812e
Shubham Bandagale <sbandagale18@gmail.com>
2021-08-16 03:02:00 +0530

    UC-3

--- src/com/bridgelabz/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation.java
@@ -5,6 +5,8 @@ import java.util.Random;
 public class GamblingSimulation {
     public static final int STAKE = 100;
     public static final int BET = 1;
+    public static final float STAKE_VALUE=0.5f;
+    public int winningAmount, losingAmount,stake;
 
     /**
      * @UC-2 :Win or Lose
@@ -12,15 +14,40 @@ public class GamblingSimulation {
     public void winOrLoss(){
         Random random = new Random();
         int value= random.nextInt(2);
-        if(value == 1)
+        if(value == 1) {
             System.out.println("WON BET");
-        else
+            stake++;
+        }
+        else {
             System.out.println("LOST BET");
+            stake--;
+        }
+    }
+
+    /**
+     * @UC-3 Resign for day if loss reached 50 and gain reached 150
+     */
+    public int resignStake(){
+        losingAmount = (int) Math.round(STAKE*STAKE_VALUE);
+        winningAmount =(int)Math.round(STAKE+(STAKE*STAKE_VALUE));
+        boolean stop = true;
+        stake = STAKE;
+        while(stop == true){
+
+            winOrLoss();
+            if(stake == losingAmount){
+                stop = false;
+            }
+            if(stake == winningAmount){
+                stop = false;
+            }
+        }
+        return stake;
     }
 
     public static void main(String[] args) {
         GamblingSimulation gamblerSimulation = new GamblingSimulation();
         System.out.println("WELCOME TO GAMBLING SIMULATION PROGRAM");
-        gamblerSimulation.winOrLoss();
+        System.out.println(gamblerSimulation.resignStake());
     }
 }
