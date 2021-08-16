a54d2558970cd676c5d09ef4cdfa792162276458
Shubham Bandagale <sbandagale18@gmail.com>
2021-08-16 12:56:25 +0530

    UC-4

--- src/com/bridgelabz/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation.java
@@ -5,49 +5,61 @@ import java.util.Random;
 public class GamblingSimulation {
     public static final int STAKE = 100;
     public static final int BET = 1;
-    public static final float STAKE_VALUE=0.5f;
-    public int winningAmount, losingAmount,stake;
+    public static final float STAKE_VALUE = 0.5f;
+    public int winningAmount, losingAmount, stake;
+    public int totalStake = 0;
 
     /**
      * @UC-2 :Win or Lose
      */
-    public void winOrLoss(){
+    public void winOrLoss() {
         Random random = new Random();
-        int value= random.nextInt(2);
-        if(value == 1) {
-            System.out.println("WON BET");
+        int value = random.nextInt(2);
+        if (value == 1)
             stake++;
-        }
-        else {
-            System.out.println("LOST BET");
+        else
             stake--;
-        }
     }
 
     /**
      * @UC-3 Resign for day if loss reached 50 and gain reached 150
      */
-    public int resignStake(){
-        losingAmount = (int) Math.round(STAKE*STAKE_VALUE);
-        winningAmount =(int)Math.round(STAKE+(STAKE*STAKE_VALUE));
+    public int resignStake() {
+        losingAmount = (int) Math.round(STAKE * STAKE_VALUE);
+        winningAmount = (int) Math.round(STAKE + (STAKE * STAKE_VALUE));
         boolean stop = true;
         stake = STAKE;
-        while(stop == true){
+        while (stop == true) {
 
             winOrLoss();
-            if(stake == losingAmount){
+            if (stake == losingAmount) {
                 stop = false;
             }
-            if(stake == winningAmount){
+            if (stake == winningAmount) {
                 stop = false;
             }
         }
         return stake;
     }
 
+    /**
+     * @UC-4 Total after playing for 20 days
+     */
+    public int giveTotal() {
+        int i;
+        for (i = 1; i <= 20; i++) {
+            System.out.println("Day " + i + "result: " + resignStake());
+            totalStake = totalStake + stake;
+            System.out.println("Total stake on day " + i + " is " + totalStake);
+        }
+        return totalStake;
+    }
+
+
     public static void main(String[] args) {
         GamblingSimulation gamblerSimulation = new GamblingSimulation();
         System.out.println("WELCOME TO GAMBLING SIMULATION PROGRAM");
-        System.out.println(gamblerSimulation.resignStake());
+
+        System.out.println("Total winnings till date: " + gamblerSimulation.giveTotal());
     }
 }
